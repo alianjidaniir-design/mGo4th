dee54a8d9193ce2b9c48f6ad171574b9b3b64128
Mahdi Anjidani <>
2025-11-19 09:24:43 +0330

    branch

--- uv/viper.go
+++ uv/viper.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
 
+func aliasNormalizeFunc(f *pflag.FlagSet, n string) pflag.NormalizedName {
+
+	switch n {
+	case "pass":
+		n = "password"
+		break
+	case "ps":
+		n = "password"
+		break
+	}
+
+	return pflag.NormalizedName(n)
 }
